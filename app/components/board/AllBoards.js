d55b936253188fd6aedd5c683e39dab8c3e9a430
Artem <nickmail@inbox.ru>
2020-09-05 23:34:22 +0400

    Add Form to the AllBoards component

--- app/components/board/AllBoards.js
+++ app/components/board/AllBoards.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from "react";
 import BoardLink from "./BoardLink";
+import Form from "../Form";
+import Button from "../Button";
 import { Link } from "react-router-dom";
 import { submitNewBoard } from "Actions/actions";
 import { connect } from "react-redux";
 
-const AllBoards = ({ boards }) => {
+const AllBoards = ({ boards, submitNewBoard }) => {
 	const [createMode, setCreateMode] = useState(false);
+
+	const goToBoards = () => {
+		setCreateMode(false);
+	};
 
 	const goToForm = (e) => {
 		e.preventDefault();
@@ -32,6 +38,16 @@ const AllBoards = ({ boards }) => {
 			{createMode ? (
 				<BoardLink>
 					<h4>Creating a board</h4>
+					<Form
+						classProps=""
+						submitAction={(title) => submitNewBoard(title)}
+						resetAction={goToBoards}
+						errorMsg="You have to name your new board"
+						placeholder="Add a new board"
+					>
+						<Button type="reset" classProps="" name="Cancel" />
+						<Button type="submit" classProps="" name="Submit" />
+					</Form>
 				</BoardLink>
 			) : (
 				<a href="#" onClick={goToForm}>
