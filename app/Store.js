a3cdb03234ccbf321b632841690704142cd6ba4b
Artem <nickmail@inbox.ru>
2020-09-06 02:07:13 +0400

    Temporarily add preloaded state

--- app/Store.js
+++ app/Store.js
@@ -5,8 +5,17 @@ import thunk from "redux-thunk";
 
 const middleware = applyMiddleware(thunk);
 
+//Preloaded state
+const preloadedState = {
+	boards: [{ id: 0, title: "First board" }],
+	lists: [{ id: 0, title: "First list", boardId: 0 }],
+	cards: [{ id: 0, title: "First cards", boardId: 0, listId: 0 }],
+};
+//
+
 const store = createStore(
 	rootReducer,
+	preloadedState,
 	composeWithDevTools(applyMiddleware(thunk))
 );
 
